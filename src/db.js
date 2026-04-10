636cfc3f97184ecaecb3b7a3ccdfc952f61b55cd
BharadwajNB <bharadwajnb23@gmail.com>
2026-04-10 14:19:39 +0530

    added ssl for railway db

--- src/db.js
+++ src/db.js
@@ -7,15 +7,17 @@ const connectionString = process.env.MYSQL_URL || process.env.DATABASE_URL;
 
 if (connectionString) {
     // Connect using a connection string
-    pool = mysql.createPool(connectionString);
-} else {
-    // Connect using individual variables
     pool = mysql.createPool({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD || process.env.DB_PASS, // Supporting both conventions
+        password: process.env.DB_PASSWORD || process.env.DB_PASS,
         database: process.env.DB_NAME,
         port: parseInt(process.env.DB_PORT) || 3306,
+
+        ssl: {
+            rejectUnauthorized: false
+        },
+
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
